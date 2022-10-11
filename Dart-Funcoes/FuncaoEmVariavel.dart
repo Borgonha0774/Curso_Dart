44c354ac2097424825b56028e5b90ecca17205d7
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-11 08:16:57 -0300

    funcões em variavel, funcões anonimas

---
+++ Dart-Funcoes/FuncaoEmVariavel.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  int result = SumFn(1, 3);
+  print("O resultado é: $result");
+
+  //add prototipo da variavel
+  int Function(int, int) SumPrototipe = SumFn;
+  print(SumPrototipe(1, 3));
+
+  //Função Anonima
+  int Function(int, int) SumAnonimous1 = (p0, p1) {
+    return p0 + p1;
+  };
+  print(SumAnonimous1(1, 2));
+
+  int Function(int, int) SumAnonimous2 = (p0, p1) => p0 + p1;
+  print(SumAnonimous2(1, 2));
+
+  int Function(int, int) MultiplayAnonimous = (p0, p1) => p0 * p1;
+
+  print(MultiplayAnonimous(SumAnonimous1(1, 3), SumAnonimous2(1, 3)));
+}
+
+int SumFn(int a, int b) {
+  return a + b;
+}
