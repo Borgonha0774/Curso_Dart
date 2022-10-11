f5164dbba9a0cbdb21b8cb1fbe170481c8f82991
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-11 16:03:16 -0300

    reduce

--- Dart-Funcoes/MapReduce.dart
+++ Dart-Funcoes/MapReduce.dart
@@ -7,8 +7,13 @@ void main(List<String> args) {
     {'nome': 'Ana', 'nota': 7.6},
     {'nome': 'Ricardo', 'nota': 6.8},
   ];
+  print("\nFunções Reduce");
+  var notas = [7.1, 7.5, 4.9, 3.2, 2.9, 8.7, 9.8];
+  double sumer(double a, double b) => a + b;
+  print(notas.reduce(sumer));
+  print(notas.reduce((acc, el) => acc + el));
 
-  print("Função map transforma uma lista e não altera a quantidade dela");
+  print("\nFunção map transforma uma lista e não altera a quantidade dela");
   String Function(Map) getName = (aluno) => aluno['nome'];
   var newName = alunos.map(getName);
   print(newName);
