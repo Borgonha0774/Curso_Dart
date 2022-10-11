f96054ced70f5e03bad32f927f0d15abe7c6dcdb
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-11 13:08:38 -0300

    metodo where

--- Dart-Funcoes/filters.dart
+++ Dart-Funcoes/filters.dart
@@ -1,5 +1,8 @@
 void main(List<String> args) {
+  print("Metodo procedural: For");
   metodoTradicionalFiltro();
+  print("\nMetodo Dart: Where");
+  metodoWhereFiltro();
 }
 
 //método procedural
@@ -10,5 +13,14 @@ void metodoTradicionalFiltro() {
   for (var nota in notas) {
     if (nota >= 7) notasBoas.add(nota);
   }
+  print(notasBoas);
+}
+
+//método where original do dart
+void metodoWhereFiltro() {
+  var notas = [8.2, 7.1, 6.6, 2.9, 10.7, 3.9, 12.8, 1.5];
+
+  bool Function(double) notasBoasFn = (double nota) => nota >= 8.3;
+  var notasBoas = notas.where((nota) => notasBoasFn(nota));
   print(notasBoas);
 }
