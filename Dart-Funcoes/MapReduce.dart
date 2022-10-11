70b72290199c964bb68fbd0374ac11fc2e61b440
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-11 15:30:04 -0300

    map

---
+++ Dart-Funcoes/MapReduce.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Mariana', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
+
+  print("Função map transforma uma lista e não altera a quantidade dela");
+  String Function(Map) getName = (aluno) => aluno['nome'];
+  var newName = alunos.map(getName);
+  print(newName);
+
+//faço a função dentro do map mas preciso fazer o casting para string
+  var result =
+      alunos.map((name) => name['nome'] as String).map((e) => e.length);
+  print(result);
+}
