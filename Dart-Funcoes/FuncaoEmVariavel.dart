b1a8c5e69f27efd750f1c70001bc1a14f6e4f82e
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-11 08:28:31 -0300

    parametros com valor default

--- Dart-Funcoes/FuncaoEmVariavel.dart
+++ Dart-Funcoes/FuncaoEmVariavel.dart
@@ -18,8 +18,16 @@ void main(List<String> args) {
   int Function(int, int) MultiplayAnonimous = (p0, p1) => p0 * p1;
 
   print(MultiplayAnonimous(SumAnonimous1(1, 3), SumAnonimous2(1, 3)));
+
+  ParametrosSemObrigacoes();
+  ParametrosSemObrigacoes(100, 200);
 }
 
 int SumFn(int a, int b) {
   return a + b;
+}
+
+//Parametros opcinais com valor default
+void ParametrosSemObrigacoes([int a = 0, int b = 0]) {
+  print("Parametros sem obrigações: ${a + b}");
 }
