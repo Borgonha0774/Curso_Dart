e7ec4c38ebc31d4a76d0957aa1f989696d805dca
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-12 08:03:16 -0300

    iniciando classes

---
+++ Dart_OOP/classData.dart
@@ -0,0 +1,22 @@
+class Data {
+  late int dia;
+  late int mes;
+  late int ano;
+}
+
+void main(List<String> args) {
+  //O new é opcional
+  var dataAniversario = new Data();
+  dataAniversario.dia = 31;
+  dataAniversario.mes = 8;
+  dataAniversario.ano = 1949;
+
+  //o tipo pode ser o da classe
+  Data dataCompra = Data();
+  dataCompra.dia = 23;
+  dataCompra.mes = 12;
+  dataCompra.ano = 2008;
+
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+}
