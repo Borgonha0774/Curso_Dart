18f0eef73ad04d5629ee48df16f45d06b5925493
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-12 08:42:54 -0300

    construcotrs

--- Dart_OOP/classData.dart
+++ Dart_OOP/classData.dart
@@ -1,24 +1,42 @@
 class Data {
   //Objetos de instancia
-  late int dia;
-  late int mes;
-  late int ano;
+  int dia;
+  int mes;
+  int? ano; //passando o ? não precisa ser inicializado
+
+  //Consturtor padrão
+  //Data() {}
+
+  //Consturtor obrigando pasagem dos parametros
+  /* Data(int dia, int mes, int ano) {
+    this.dia = dia;
+    this.mes = mes;
+    this.ano = ano;
+  } */
+
+  //Simplificando o construtor os colchetes não obrigam a passagem de parametros no construtor
+  Data([this.dia = 0, this.mes = 0, this.ano]);
 
   //Metodos
   String getData() {
     return ("${dia}/${mes}/${ano}");
   }
+
+  //Metodo chamando outro metodo
+  String toString() {
+    return getData();
+  }
 }
 
 void main(List<String> args) {
   //O new é opcional
-  var dataAniversario = new Data();
-  dataAniversario.dia = 31;
+  var dataAniversario = new Data(31, 8, 1949);
+  /* dataAniversario.dia = 31;
   dataAniversario.mes = 8;
-  dataAniversario.ano = 1949;
+  dataAniversario.ano = 1949; */
 
   //o tipo pode ser o da classe
-  Data dataCompra = Data();
+  Data dataCompra = Data(1, 1, 1970);
   dataCompra.dia = 23;
   dataCompra.mes = 12;
   dataCompra.ano = 2008;
@@ -27,4 +45,7 @@ void main(List<String> args) {
   print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}"); */
   print(dataAniversario.getData());
   print(dataCompra.getData());
+
+//Metodo chamndo outro
+  print(dataCompra.toString());
 }
