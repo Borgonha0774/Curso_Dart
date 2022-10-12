907233007346f69946214f021b92bac903afd704
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-12 08:59:20 -0300

    named constructors

--- Dart_OOP/classData.dart
+++ Dart_OOP/classData.dart
@@ -1,7 +1,7 @@
 class Data {
   //Objetos de instancia
-  int dia;
-  int mes;
+  late int dia;
+  late int mes;
   int? ano; //passando o ? não precisa ser inicializado
 
   //Consturtor padrão
@@ -16,6 +16,12 @@ class Data {
 
   //Simplificando o construtor os colchetes não obrigam a passagem de parametros no construtor
   Data([this.dia = 0, this.mes = 0, this.ano]);
+  //Construtor nomeado
+  Data.com({this.dia = 0, this.mes = 0, this.ano});
+  Data.ultimoDiaDoAno(this.ano) {
+    dia = 31;
+    mes = 12;
+  }
 
   //Metodos
   String getData() {
@@ -46,6 +52,13 @@ void main(List<String> args) {
   print(dataAniversario.getData());
   print(dataCompra.getData());
 
-//Metodo chamndo outro
+  //Metodo chamndo outro
   print(dataCompra.toString());
+
+  //Construtor nomeado
+  var newData = Data.com(dia: 28, mes: 07, ano: 1974);
+  print(newData.toString());
+  print(newData.getData());
+
+  print(Data.ultimoDiaDoAno(2028));
 }
